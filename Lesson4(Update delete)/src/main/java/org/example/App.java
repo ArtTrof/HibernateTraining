c458ab0c0d7b8b757ab0180fbe72d07a053214fb
Artem Trofymenko <randallsng@gmail.com>
2023-01-31 17:18:36 +0100

    msg

--- Lesson4(Update delete)/src/main/java/org/example/App.java
+++ Lesson4(Update delete)/src/main/java/org/example/App.java
@@ -6,8 +6,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.util.List;
-
 /**
  * Hello world!
  */
@@ -21,15 +19,16 @@ public class App {
 
         try {
             session.beginTransaction();
-//
-//            List<Person>people = session.createQuery("From Person where name like 'P%'").getResultList();
-//            for (Person person:people){
-//                System.out.println(person.toString());
-//            }
-//set all name on test where age<30
-//             session.createQuery("update Person  set name='TEST' where age<30").executeUpdate();
-//delete person where age <30
-            session.createQuery("delete Person  where age<30").executeUpdate();
+//update person
+//            Person person = session.get(Person.class,2);
+//            person.setName("New name");
+//delete person
+//            Person person = session.get(Person.class,2);
+//            session.delete(person);
+//hibernate remembers id of created person
+            Person person = new Person("some name",20);
+            session.save(person);
+            System.out.println(person.getId());
 
             session.getTransaction().commit();
         } finally {
